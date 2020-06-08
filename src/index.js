c1e0a51c1ae4c14484cb4c5bb9efe617fdb75b3e
abidurr <42abid@gmail.com>
2020-06-08 16:55:24 -0400

    Now renaming a todo won't rerender counters

--- src/index.js
+++ src/index.js
@@ -4,79 +4,91 @@ import { types } from "mobx-state-tree";
 import { observer } from "mobx-react";
 import { values } from "mobx";
 
-let id = 1;
-const randomId = () => ++id;
+const randomId = () => Math.floor(Math.random() * 1000).toString(36);
 
 const Todo = types
-    .model({
-        id: types.identifierNumber,
-        name: types.optional(types.string, ""),
-        done: types.optional(types.boolean, false),
-    })
-    .actions((self) => {
-        function setName(newName) {
-            self.name = newName;
-        }
+  .model({
+    name: types.optional(types.string, ""),
+    done: types.optional(types.boolean, false)
+  })
+  .actions(self => {
+    function setName(newName) {
+      self.name = newName;
+    }
 
-        function toggle() {
-            self.done = !self.done;
-        }
+    function toggle() {
+      self.done = !self.done;
+    }
 
-        return { setName, toggle };
-    });
+    return { setName, toggle };
+  });
 
 const User = types.model({
-    name: types.optional(types.string, ""),
+  name: types.optional(types.string, "")
 });
 
 const RootStore = types
-    .model({
-        users: types.map(User),
-        todos: types.optional(types.map(Todo), {}),
-    })
-    .actions((self) => {
-        function addTodo(id, name) {
-            self.todos.set(id, Todo.create({ id, name }));
-        }
+  .model({
+    users: types.map(User),
+    todos: types.map(Todo)
+  })
+  .views(self => ({
+    get pendingCount() {
+      return values(self.todos).filter(todo => !todo.done).length;
+    },
+    get completedCount() {
+      return values(self.todos).filter(todo => todo.done).length;
+    }
+  }))
+  .actions(self => {
+    function addTodo(id, name) {
+      self.todos.set(id, Todo.create({ name }));
+    }
 
-        return { addTodo };
-    });
+    return { addTodo };
+  });
 
 const store = RootStore.create({
-    users: {},
-    todos: {
-        "1": {
-            id: id,
-            name: "Eat a cake",
-            done: true,
-        },
-    },
+  users: {},
+  todos: {
+    "1": {
+      name: "Eat a cake",
+      done: true
+    }
+  }
 });
 
-const TodoView = observer((props) => (
-    <div>
-        <input
-            type="checkbox"
-            checked={props.todo.done}
-            onChange={(e) => props.todo.toggle()}
-        />
-        <input
-            type="text"
-            value={props.todo.name}
-            onChange={(e) => props.todo.setName(e.target.value)}
-        />
-    </div>
+const TodoView = observer(props => (
+  <div>
+    <input
+      type="checkbox"
+      checked={props.todo.done}
+      onChange={e => props.todo.toggle()}
+    />
+    <input
+      type="text"
+      value={props.todo.name}
+      onChange={e => props.todo.setName(e.target.value)}
+    />
+  </div>
 ));
 
-const AppView = observer((props) => (
-    <div>
-        <button onClick={(e) => props.store.addTodo(randomId(), "New Task")}>
-            Add Task
-        </button>
-        {values(props.store.todos).map((todo) => (
-            <TodoView todo={todo} />
-        ))}
-    </div>
+const TodoCounterView = observer(props => (
+  <div>
+    {props.store.pendingCount} pending, {props.store.completedCount} completed
+  </div>
+));
+
+const AppView = observer(props => (
+  <div>
+    <button onClick={e => props.store.addTodo(randomId(), "New Task")}>
+      Add Task
+    </button>
+    {values(props.store.todos).map(todo => (
+      <TodoView todo={todo} />
+    ))}
+    <TodoCounterView store={props.store} />
+  </div>
 ));
 
 render(<AppView store={store} />, document.getElementById("root"));
