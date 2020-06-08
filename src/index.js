5b6605ef6d60edbad2fc013a6c502ee2565c33d9
abidurr <42abid@gmail.com>
2020-06-07 20:26:51 -0400

    Modifying data

--- src/index.js
+++ src/index.js
@@ -1,24 +1,51 @@
+import "./index.css";
 import React from "react";
 import { render } from "react-dom";
 import { types, getSnapshot } from "mobx-state-tree";
-import { observer } from "mobx-react";
 
-const Todo = types.model({
-  name: "",
-  done: false
-});
+const Todo = types
+    .model({
+        name: types.optional(types.string, ""),
+        done: types.optional(types.boolean, false)
+    })
+    .actions(self => {
+        function setName(newName) {
+            self.name = newName;
+        }
+        function toggle() {
+            self.done = !self.done;
+        }
+        return { setName, toggle }
+    });
 
 const User = types.model({
-  name: ""
+    name: types.optional(types.string, ""),
 });
 
-const john = User.create();
-const eat = Todo.create({ name: "eat" });
+const RootStore = types
+    .model({
+        users: types.map(User),
+        todos: types.optional(types.map(Todo), {})
+    })
+    .actions(self => {
+        function addTodo(id, name) {
+            self.todos.set(id, Todo.create({ name }))
+        }
+        return { addTodo }
+    });
+
+const store = RootStore.create({
+  users: {}
+})
+
+store.addTodo(1, "GIVE ME COFFEE")
+store.todos.get(1).toggle()
+
+
 render(
-  <div>
-    John: {JSON.stringify(getSnapshot(john))}
-    <br />
-    Eat TODO: {JSON.stringify(getSnapshot(eat))}
-  </div>,
-  document.getElementById("root")
+    <div>
+        Store: {JSON.stringify(getSnapshot(store))}
+        <br />
+    </div>,
+    document.getElementById("root")
 );
