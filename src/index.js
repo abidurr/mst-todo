5065045f6bf524692aa18fde46afaa961878927f
abidurr <42abid@gmail.com>
2020-06-08 16:57:46 -0400

    Model views for sorting todos

--- src/index.js
+++ src/index.js
@@ -38,6 +38,9 @@ const RootStore = types
     },
     get completedCount() {
       return values(self.todos).filter(todo => todo.done).length;
+    },
+    getTodosWhereDoneIs(done) {
+      return values(self.todos).filter(todo => todo.done === done);
     }
   }))
   .actions(self => {
